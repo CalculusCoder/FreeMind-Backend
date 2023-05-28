188f69b5d8280e261e5b9eaa0bfa2dee36230af0
CalculusCoder <jaredgomez0812@gmail.com>
2023-05-27 20:06:15 -0400

    configuring database ssl

--- configuration/config.ts
+++ configuration/config.ts
@@ -1,12 +1,32 @@
-export {};
-export const db = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  // user: "postgres",
-  // password: "Emanuel12",
-  // database: "postgres",
-  // host: "localhost",
-  // port: 5432,
-};
+// export {};
+// export const db = {
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   user: "postgres",
+//   password: "Emanuel12",
+//   database: "postgres",
+//   host: "localhost",
+//   port: 5432,
+// };
+
+import { PoolConfig } from "pg";
+
+let db: PoolConfig;
+
+if (process.env.NODE_ENV === "production") {
+  db = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
+  db = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
+  };
+}
+
+export { db };
--- dist/configuration/config.js
+++ dist/configuration/config.js
@@ -1,15 +1,32 @@
 "use strict";
+// export {};
+// export const db = {
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   user: "postgres",
+//   password: "Emanuel12",
+//   database: "postgres",
+//   host: "localhost",
+//   port: 5432,
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
-exports.db = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-    // user: "postgres",
-    // password: "Emanuel12",
-    // database: "postgres",
-    // host: "localhost",
-    // port: 5432,
-};
+let db;
+exports.db = db;
+if (process.env.NODE_ENV === "production") {
+    exports.db = db = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    };
+}
+else {
+    exports.db = db = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: false,
+    };
+}
 //# sourceMappingURL=config.js.map
