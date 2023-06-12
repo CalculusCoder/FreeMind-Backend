4edc9674f7661316b335c7fad4ce70f0b9e82c50
CalculusCoder <jaredgomez0812@gmail.com>
2023-06-12 12:42:30 -0400

    debugging email receipt

--- dist/webhook.js
+++ dist/webhook.js
@@ -8,35 +8,44 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.webhookHandler = void 0;
 const db_1 = require("./db/db");
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const ejs_1 = __importDefault(require("ejs"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
-// const transport = nodemailer.createTransport({
-//   service: "Gmail",
-//   auth: {
-//     user: process.env.GOOGLE_EMAIL,
-//     pass: process.env.GOOGLE_PASSWORD
-//   }
-// })
-// async function sendReceiptEmail(userEmail: string, receipt: any) {
-//   const filePath = path.join(__dirname, 'views/receipt.ejs');
-//   const compiled = ejs.compile(fs.readFileSync(filePath, 'utf8'));
-//   console.log(filePath);
-//   const mailOptions = {
-//     from: process.env.EMAIL_USERNAME,
-//     to: userEmail,
-//     subject: 'Your Receipt',
-//     html: compiled({ receipt: receipt }) // pass data to template
-//   };
-//   return transport.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       return console.log(error);
-//     }
-//     console.log('Message sent: %s', info.messageId);
-//   });
-// }
+const transport = nodemailer_1.default.createTransport({
+    service: "Gmail",
+    auth: {
+        user: process.env.GOOGLE_EMAIL,
+        pass: process.env.GOOGLE_PASSWORD
+    }
+});
+function sendReceiptEmail(userEmail, receipt) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const filePath = path_1.default.join(__dirname, 'views/receipt.ejs');
+        const compiled = ejs_1.default.compile(fs_1.default.readFileSync(filePath, 'utf8'));
+        console.log(filePath);
+        const mailOptions = {
+            from: process.env.EMAIL_USERNAME,
+            to: userEmail,
+            subject: 'Your Receipt',
+            html: compiled({ receipt: receipt }) // pass data to template
+        };
+        return transport.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+        });
+    });
+}
 function webhookHandler(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const stripeSignature = req.headers["stripe-signature"];
@@ -78,7 +87,7 @@ function webhookHandler(req, res) {
             console.log("Invoice payment was successful:", invoice.id);
             const userEmail = invoice.customer_email;
             const customerId = invoice.customer;
-            // sendReceiptEmail(userEmail, invoice);
+            sendReceiptEmail(userEmail, invoice);
             const subscription = yield stripe.subscriptions.retrieve(event.data.object.subscription);
             const newExpirationDate = new Date(subscription.current_period_end * 1000);
             newExpirationDate.setHours(newExpirationDate.getHours() + 1);
