eed0ae494ab3ab2535132ed130bc032f910a83f5
CalculusCoder <jaredgomez0812@gmail.com>
2023-11-03 23:26:10 -0400

    changing freemindrecovery website to www.

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ if (port == null || port == "") {
 }
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://freemindrecovery.com"],
+  origin: ["http://localhost:3000", "https://www.freemindrecovery.com"],
   credentials: true,
 };
 
